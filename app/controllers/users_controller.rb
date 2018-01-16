c9383ed2b43914ea467e229e15824322220d9161
Travis Witherington <twitherington@covermymeds.com>
2018-01-16 10:46:39 -0600

    changed && to and

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,14 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user && !current_user.admin?
+    if current_user != @user and !current_user.admin?
       flash[:danger] = 'You can only edit your own account.'
       redirect_to root_path
     end
   end
 
   def require_admin
-    if logged_in? && !current_user.admin?
+    if logged_in? and !current_user.admin?
       flash[:danger] = 'Only admin users can perform that action'
       redirect_to root_path
     end
