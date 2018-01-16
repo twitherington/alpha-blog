1b46d6d21ea9c1eac2cbef9a762e66de2067c66c
Travis Witherington <twitherington@covermymeds.com>
2018-01-16 10:36:36 -0600

    added admin user code and fixed visited link css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109203454) do
+ActiveRecord::Schema.define(version: 20180115223012) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 20180109203454) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
