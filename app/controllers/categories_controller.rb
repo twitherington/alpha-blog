ac6f145e3fa9fe0640093df786290fabec089df7
Travis Witherington <twitherington@covermymeds.com>
2018-01-17 16:57:27 -0500

    fixed rails 5 test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,6 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
   end
 
   def create
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -12,6 +12,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
@@ -24,7 +25,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
  test 'should redirect create when admin not logged in' do
    assert_no_difference 'Category.count' do
-     post :create, category: { name: 'sports' }
+     post categories_path, params: { category: { name: 'sports' }}
    end
    assert_redirected_to categories_path
  end
